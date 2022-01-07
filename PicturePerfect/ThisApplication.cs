c89538923bd03e0f74c03c106c0073e5216ad65d
Ludwig Sembach <ludwig.sembach@outlook.com>
2022-01-07 21:20:28 +0100

    MealPlanner.NET carry over

---
+++ PicturePerfect/ThisApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicturePerfect
+{
+    internal class ThisApplication
+    {
+        public static string DarkColor = "#232323"; // carbon
+        public static string MediumColor = "Gray";
+        public static string LightColor = "WhiteSmoke";
+        public static string LightFontColor = "WhiteSmoke";
+        public static string DarkContrastColor = "#2E3033"; // rich gray
+    }
+}
--- PicturePerfect/ViewModels/MainWindowViewModel.cs
+++ PicturePerfect/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace PicturePerfect.ViewModels
@@ -7,5 +8,19 @@ namespace PicturePerfect.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+        public static string Date => DateTime.Today.ToString("yyyy-MM-dd");
+        public static string CalendarWeek => "CW " + ISOWeek.GetWeekOfYear(DateTime.Now).ToString();
+
+        # region Color properties
+        public static string DarkColor => ThisApplication.DarkColor;
+        public static string MediumColor => ThisApplication.MediumColor;
+        public static string LightColor => ThisApplication.LightColor;
+        public static string LightFontColor => ThisApplication.LightFontColor;
+        public static string DarkContrastColor => ThisApplication.DarkContrastColor;
+        #endregion
+
+        #region Fonts
+        public static int LargeFontSize => 23;
+        #endregion
     }
 }
