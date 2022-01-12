df1326d8450d8d71a9057e069bc273e41915f484
Ludwig Sembach <ludwig.sembach@outlook.com>
2022-01-12 12:13:56 +0100

    project file fixed

--- PicturePerfect/Models/ProjectFile.cs
+++ PicturePerfect/Models/ProjectFile.cs
@@ -10,10 +10,29 @@ namespace PicturePerfect.Models
 {
     internal class ProjectFile
     {
-        public string ProjectOwner { get; private set; }
-        public DateTime CreationDate { get; private set; }
+        #region General
+        public string ProjectName { get; set; } = string.Empty;
+        public string ProjectOwner { get; private set; } = string.Empty;
+        public DateTime CreationDate { get; private set; } = DateTime.Now;
         public string Notes { get; private set; } = string.Empty;
+        public string ProjectFolder { get; private set; } = string.Empty;
+        public string DatabasePath { get; private set; } = string.Empty;
+        private string Release { get; set; } = string.Empty;
+        #endregion
 
+        #region Theme
+        public string DarkColor { get; set; } = ThisApplication.DarkColorDefault;
+        public string MediumColor { get; set; } = ThisApplication.MediumColorDefault;
+        public string LightColor { get; set; } = ThisApplication.LightColorDefault;
+        public string LightFontColor { get; set; } = ThisApplication.LightFontColorDefault;
+        public string DarkContrastColor { get; set; } = ThisApplication.DarkContrastColorDefault;
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance of the class project file.
+        /// This json file is used to store all relevant setings for a project.
+        /// The file type is .ppp (PicturePerfectPoject-File)
+        /// </summary>
         public ProjectFile()
         {
 
@@ -22,16 +41,25 @@ namespace PicturePerfect.Models
         /// <summary>
         /// Method to create a new project file. The file is saved to the selected folder.
         /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
         /// <returns>Return the project file.</returns>
-        public static ProjectFile New(string path)
+        public static ProjectFile New(string path, string name)
         {
-            //UserSettings.Default.PathToProjectFile = path;
-            ProjectFile file = new();
-            file.ProjectOwner = Environment.UserName;
-            file.CreationDate = DateTime.Now;
+            // new project file object
+            ProjectFile file = new()
+            {
+                Release = ThisApplication.ApplicationVersion,
+                ProjectName = name,
+                ProjectOwner = Environment.UserName,
+                CreationDate = DateTime.Now,
+                ProjectFolder = new FileInfo(path).Directory.FullName,
+                DatabasePath = Path.Combine(new FileInfo(path).Directory.FullName, "database.sqlite")
+            };
 
-            //string jsonString = JsonConvert.SerializeObject(file);
-            //File.WriteAllText(UserSettings.Default.PathToProjectFile, jsonString);
+            // save to json file
+            string jsonString = JsonConvert.SerializeObject(file);
+            File.WriteAllText(path, jsonString);
 
             return file;
         }
@@ -39,14 +67,43 @@ namespace PicturePerfect.Models
         /// <summary>
         /// Method to load the project file.
         /// </summary>
+        /// <param name="path"></param>
         /// <returns>Returns the project file object.</returns>
-        public static ProjectFile Load()
+        public static ProjectFile Load(string path)
         {
-            //string jsonString = File.ReadAllText(UserSettings.Default.PathToProjectFile);
-            //ProjectFile file = JsonConvert.DeserializeObject<ProjectFile>(jsonString);
-            //return file;
+            // load current file
+            string jsonString = File.ReadAllText(path);
+            ProjectFile file = JsonConvert.DeserializeObject<ProjectFile>(jsonString);
 
-            return new ProjectFile();
+            // create new file object and carry over the information
+            // this avoids compatibility issues in case the properties of this class are changed
+            // update project file
+            ProjectFile newFile = new ProjectFile()
+            {
+                Release = ThisApplication.ApplicationVersion,
+                ProjectName = file.ProjectName,
+                ProjectOwner = file.ProjectOwner,
+                CreationDate = file.CreationDate,
+                Notes = file.Notes,
+                ProjectFolder = file.ProjectFolder,
+                DatabasePath = file.DatabasePath
+                // add new properties and set the values!
+            };
+
+            return newFile;
+        }
+
+        /// <summary>
+        /// Method to provide a default project file object at application startup.
+        /// </summary>
+        /// <returns>Return the ProjectFile object.</returns>
+        public static ProjectFile AtStartup()
+        {
+            ProjectFile file = new()
+            {
+                ProjectName = "Load project"
+            };
+            return file;
         }
     }
 }
--- PicturePerfect/ThisApplication.cs
+++ PicturePerfect/ThisApplication.cs
@@ -7,14 +7,37 @@ using System.Threading.Tasks;
 
 namespace PicturePerfect
 {
-    internal class ThisApplication
+    internal static class ThisApplication
     {
+
         public static string DarkColor = "#232323"; // carbon
         public static string MediumColor = "Gray";
         public static string LightColor = "WhiteSmoke";
         public static string LightFontColor = "WhiteSmoke";
         public static string DarkContrastColor = "#2E3033"; // rich gray
 
-        public static ProjectFile ProjectFile { get; set; }
+        #region Default theme colors
+        public static string DarkColorDefault = "#232323"; // carbon
+        public static string MediumColorDefault = "Gray";
+        public static string LightColorDefault = "WhiteSmoke";
+        public static string LightFontColorDefault = "WhiteSmoke";
+        public static string DarkContrastColorDefault = "#2E3033"; // rich gray
+        #endregion
+
+        /// <summary>
+        /// Get the current application Version.
+        /// </summary>
+        public static string ApplicationVersion { get; } = "V0.1.0";
+        /// <summary>
+        /// Get the array of alle released application versions.
+        /// </summary>
+        public static string[] ApplicationVersionList { get; } = new string[] { "V0.1.0" };
+
+        /// <summary>
+        /// Get or set the currently loaded project file.
+        /// </summary>
+        public static ProjectFile ProjectFile { get; set; } = ProjectFile.AtStartup();
+
+
     }
 }
