252d9407613801e494e5c3da9ba8cc9a01c43ce6
Ludwig Sembach <ludwig.sembach@outlook.com>
2022-01-17 23:01:13 +0100

    themes in thisapplication changed to properties

--- PicturePerfect/ThisApplication.cs
+++ PicturePerfect/ThisApplication.cs
@@ -10,19 +10,25 @@ namespace PicturePerfect
     internal static class ThisApplication
     {
         #region Default theme colors
-        public static string DarkColorDefault = "#232323"; // carbon
-        public static string MediumColorDefault = "Gray";
-        public static string LightColorDefault = "WhiteSmoke";
-        public static string LightFontColorDefault = "WhiteSmoke";
-        public static string DarkContrastColorDefault = "#2E3033"; // rich gray
+        public static string DarkColorDefault => "#232323"; // carbon
+        public static string MediumColorDefault => "Gray";
+        public static string LightColorDefault => "WhiteSmoke";
+        public static string LightFontColorDefault => "WhiteSmoke";
+        public static string DarkContrastColorDefault => "#2E3033"; // rich gray
         #endregion
 
         /// <summary>
         /// Get the current application Version.
         /// </summary>
         public static string ApplicationVersion { get; } = "V0.1.0";
-        public static string ApplicationName = "PicturePerfect";
-        public static string BuildDate = "2022-01-15";
+        /// <summary>
+        /// Get the application name.
+        /// </summary>
+        public static string ApplicationName { get; } = "PicturePerfect";
+        /// <summary>
+        /// Get the application build date.
+        /// </summary>
+        public static string BuildDate { get; } = "2022-01-15";
 
         /// <summary>
         /// Get or set the currently loaded project file.
